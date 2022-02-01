4a5b56e76d42cec5c05b50c439cab9f806643d73
Jason Wang <jasonherngwang@gmail.com>
2022-01-31 22:23:08 -0800

    Completed Debugging problems.

--- debugging/number_guessing_game.rb
+++ debugging/number_guessing_game.rb
@@ -1,6 +1,55 @@
+=begin
+Diagnosis
+- Chosen number seems to be between 2 integers. One integer returns too small,
+  but going up by 1 returns too large.
+- Too big and too small are incorrect.
+- Allows more than 10 guesses.
+
+Code review
+- Recursive call at the end causes the winning number to be sampled again. Each
+  iteration has a different winning number.
+  - The more guess wrong, the higher the recursive stack grows.
+- Does not break even if the correct number is guessed.
+
+=end
+require 'pry'
+
 def valid_integer?(string)
   string.to_i.to_s == string
 end
+
+def guess_number(max_number, max_attempts)
+  winning_number = (1..max_number).to_a.sample
+  attempts = 0
+  # binding.pry
+  puts "Winning: #{winning_number}"
+
+  loop do
+    attempts += 1
+    break if attempts > max_attempts
+    puts "Attempt: #{attempts}"
+
+    input = nil
+    until valid_integer?(input)
+      print 'Make a guess: '
+      input = gets.chomp
+    end
+
+    guess = input.to_i
+
+    if guess == winning_number
+      puts 'Yes! You win.'
+    else
+      puts 'Nope. Too small.' if guess < winning_number
+      puts 'Nope. Too big.'   if guess > winning_number
+
+      # Try again:
+      guess_number(max_number, max_attempts)
+    end
+  end
+end
+
+# guess_number(10, 3)
 
 def guess_number(max_number, max_attempts)
   winning_number = (1..max_number).to_a.sample
@@ -20,12 +69,10 @@ def guess_number(max_number, max_attempts)
 
     if guess == winning_number
       puts 'Yes! You win.'
+      break
     else
       puts 'Nope. Too small.' if guess < winning_number
       puts 'Nope. Too big.'   if guess > winning_number
-
-      # Try again:
-      guess_number(max_number, max_attempts)
     end
   end
 end
---
+++ debugging/whats_wrong.rb
@@ -0,0 +1,12 @@
+=begin
+Need to add parentheses or else `p` binds to arr.sort stronger than arr.sort
+binds to the block.
+=end
+
+arr = ["9", "8", "7", "10", "11"]
+p (arr.sort do |x, y|
+    y.to_i <=> x.to_i
+  end)
+
+# Expected output: ["11", "10", "9", "8", "7"] 
+# Actual output: ["10", "11", "7", "8", "9"] 
